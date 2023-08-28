30ad3bfc376eb1c4341301b59a7e34effd8ee89a
Jguerra47 <jguerra47canada@gmail.com>
2023-08-28 12:56:26 -0500

    Create Flask server

---
+++ reto_2/src/api_gateway/gateway.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from routes import file_services_bp
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    return 'pong'
+
+app.register_blueprint(file_services_bp)
+
+load_dotenv()
+
+PORT = os.getenv('PORT')
+
+def run():
+    app.run(host='0.0.0.0',debug=True, port = PORT)
